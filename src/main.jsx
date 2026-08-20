44efd935fdcbaf8fb74748937dbe6ea4d121e292
LuisLipinski <98597288+LuisLipinski@users.noreply.github.com>
2026-08-20 10:30:05 -0300

    Mount shared toast provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import MobileHelp from './components/MobileHelp.jsx'
+import { ToastProvider } from './components/ToastProvider.jsx'
 import './styles.css'
 import './phase1.css'
 import './phase1-finance.css'
@@ -10,7 +11,9 @@ import './react-tools.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <MobileHelp />
+    <ToastProvider>
+      <App />
+      <MobileHelp />
+    </ToastProvider>
   </StrictMode>,
 )
