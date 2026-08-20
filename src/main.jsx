d9b82b4e9c623410ef7ecc79dab55d07d10736cc
LuisLipinski <98597288+LuisLipinski@users.noreply.github.com>
2026-08-20 00:18:43 -0300

    Mount mobile contextual help sheet

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import MobileHelp from './components/MobileHelp.jsx'
 import './styles.css'
 import './phase1.css'
 import './phase1-finance.css'
@@ -10,5 +11,6 @@ import './react-tools.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <MobileHelp />
   </StrictMode>,
 )
