5fb10596257e64e0538ecee76396af54f75df0c3
LuisLipinski <98597288+LuisLipinski@users.noreply.github.com>
2026-08-20 15:29:13 -0300

    Load career chart styles

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './styles.css'
 import './phase1.css'
 import './phase1-finance.css'
 import './phase1-career.css'
+import './phase1-charts.css'
 import './react-tools.css'
 
 createRoot(document.getElementById('root')).render(
